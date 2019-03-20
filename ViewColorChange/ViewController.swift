6e4084457b244a908b956fff919157f09a9e82e9
dit02 <dit@imac-7.local>
2019-03-20 15:29:44 +0900

    fourth Commit

--- ViewColorChange/ViewController.swift
+++ ViewColorChange/ViewController.swift
@@ -5,23 +5,41 @@
 //  Created by dit02 on 2019. 3. 20..
 //  Copyright © 2019년 dit02. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+   
     @IBOutlet weak var firstView: UIView!
     @IBOutlet weak var secondView: UIView!
     @IBOutlet weak var thirdView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor=UIColor.lightGray
-        firstView.backgroundColor=UIColor.red;
-        secondView.backgroundColor=UIColor.blue;
-        thirdView.backgroundColor=UIColor.green;
+        self.view.backgroundColor=UIColor.brown;
     }
-
+    @IBAction func changeRed(_ sender: UIButton) {
+        if(firstView.isHidden==false){
+            firstView.isHidden=true
+        }else if(firstView.isHidden==true){
+            firstView.isHidden=false
+        }
+    }
+    @IBAction func changeColor(_ sender: UIButton) {
+        secondView.backgroundColor=UIColor.white;
+        if(secondView.isHidden==false){
+            secondView.isHidden=true
+        }else if(secondView.isHidden==true){
+            secondView.isHidden=false
+        }
+    }
+    @IBAction func changeColor2(_ sender: UIButton) {
+        if(thirdView.isHidden==false){
+            thirdView.isHidden=true
+        }else if(thirdView.isHidden==true){
+            thirdView.isHidden=false
+        }
+    }
+    
 
 }
 
